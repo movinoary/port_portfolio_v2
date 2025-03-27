b58173aff8c5dd1c4c131b0a7ad23d1dfb3651d0
movinoary <97898853+movinoary@users.noreply.github.com>
2025-03-27 20:26:27 +0700

    update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,35 @@ import type { Metadata } from "next";
 import Nav from "@/components/nav";
 
 export const metadata: Metadata = {
-  title: "Portofolio",
-  description: "portofolio mohammad vino arystio",
+  title: "Portofolio Vino Arystio",
+  description: "Portofolio Mohammad Vino Arystio - Developer & Designer",
+  keywords: [
+    "Vino Arystio",
+    "Mohammad Vino Arystio",
+    "Portofolio",
+    "Developer",
+  ],
+  openGraph: {
+    title: "Portofolio Vino Arystio",
+    description: "Portofolio Mohammad Vino Arystio - Developer & Designer",
+    url: "https://portfolio-vo.vercel.app/",
+    type: "website",
+    siteName: "Portofolio Vino Arystio",
+    images: [
+      {
+        url: "https://portfolio-vo.vercel.app/",
+        width: 1200,
+        height: 630,
+        alt: "Portofolio Vino Arystio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portofolio Vino Arystio",
+    description: "Portofolio Mohammad Vino Arystio - Developer & Designer",
+    images: ["https://portfolio-vo.vercel.app//og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
