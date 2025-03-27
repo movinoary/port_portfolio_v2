8cac342d25c9ae9aa0cd94ecb506294cca498805
movinoary <97898853+movinoary@users.noreply.github.com>
2025-03-27 20:29:07 +0700

    update version

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>v.2.0 © Copyright 2025 VO</p>
+      <p>v.2.3 © Copyright 2025 VO</p>
     </footer>
   );
 };
